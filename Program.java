3cfe89b7c41f3833a20617f1e3151196c2e5ecb7
LeonardoBigShot <58840911+LeonardoBigShot@users.noreply.github.com>
2019-12-15 18:14:42 +0300

    Update and rename class program to Program.java

---
+++ Program.java
@@ -0,0 +1,6 @@
+public class Program{
+    public static void main(){
+        Lab first = new Lab("Sport",new Subject("Push up 10 times"), 1);
+        RGZ second = new RGZ("Gaming",new Subject("Kick the keyboard"), 2);
+    }
+}
