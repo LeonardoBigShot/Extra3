2ad42dbd90fde7ae815ac11478fd9fb7fa5f3418
LeonardoBigShot <58840911+LeonardoBigShot@users.noreply.github.com>
2019-12-15 18:17:17 +0300

    Update RGZ.java

--- RGZ.java
+++ RGZ.java
@@ -1,5 +1,5 @@
 class Rgz extends OrderedTask{
-    public RGZ(String Subject, Subject quest, int OrderedTask){
+    public Rgz(String Subject, Subject quest, int OrderedTask){
         super(Subject,quest,OrderedTask);
         this.number = number;
     }
